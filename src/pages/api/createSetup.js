409bc77cfed1efd3fd12f5da4da0ec1c45065b3a
JoJoGong <ugong@uw.edu>
2023-03-07 16:54:51 -0800

    edit createSetup

--- src/pages/api/createSetup.js
+++ src/pages/api/createSetup.js
@@ -4,25 +4,36 @@ import db from "../../../firebaseConfig/firebaseConfig.js";
 export default function handler(req, res) {
   console.log(req.body);
   try {
-    console.log("tried pushing new data to database");
     const data = JSON.parse(req.body);
     const dictFields = String(data.dictFields).slice(0, String(data.dictFields).length - 1);
     var _uid = 'user1';
+    // var langData = {
+    //   description: data.language_desc,
+    //   dictionary_fields: dictFields,
+    //   name: data.language_name,
+    //   uid: _uid,
+    // }
     var langData = {
-      description: data.language_desc,
-      dictionary_fields: dictFields,
-      name: data.language_name,
+      description: "testing lang1",
+      dictionary_fields: '"Orthographic forms","Keystrokes for orthography","Head word","Inflected forms","Pronunciation","Proto-form","Politeness-register scale","English definition"',
+      name: "testing lang1",
       uid: _uid,
+      dict: "",
     }
     const newLangKey = db.ref().child('languages').push().key;
 
     const updates = {}
     updates['/languages/' + newLangKey] = langData;
-    updates['/users/' + uid + '/lid/'] = newLangKey;
+    updates['/users/' + _uid + '/lid/'] = newLangKey;
 
-    db.ref().update(updates);
+    db.ref().update(updates).then(() => {
+      console.log("Data pushed successfully");
+    })
+    .catch((error) => {
+      console.error("Error pushing data:", error);
 
-    console.log("Sent dictionary data to the database!");
+    });
+
     res.status(200).json("Sent dictionary data to the database!");
   } catch (err) {
     res.status(500).json({ error: "failed to load data" });
