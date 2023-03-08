b38504ebddd5ccc242df68e74fd58a1b22d2657a
ruriz <lingrz2@uw.edu>
2023-03-07 23:14:39 -0800

    initialization changed, signup added

--- src/pages/api/createSetup.js
+++ src/pages/api/createSetup.js
@@ -1,4 +1,6 @@
-import db from "../../../firebaseConfig/firebaseConfig.js";
+import {db} from "../../../firebaseConfig/firebaseAdmin.js";
+import { ref, child, push, update } from "firebase/database";
+
 
 // To save dictionary setup data in the database
 export default function handler(req, res) {
@@ -13,18 +15,22 @@ export default function handler(req, res) {
       dictionary_fields: dictFields,
       name: data.language_name,
       uid: _uid,
+      dict: "",
     }
     console.log("langData", langData)
-    const newLangKey = db.ref().child('languages').push().key;
+    const newLangKey = push(child(ref(db),'languages')).key;
 
     const updates = {}
     updates['/languages/' + newLangKey] = langData;
     updates['/users/' + _uid + '/lid/'] = newLangKey;
-    console.log("updates", updates)
 
-    db.ref().update(updates);
+    update(ref(db), updates).then(() => {
+      console.log("Data pushed successfully");
+    })
+    .catch((error) => {
+      console.error("Error pushing data:", error);
+    });
 
-    console.log("Sent dictionary data to the database!");
     res.status(200).json("Sent dictionary data to the database!");
   } catch (err) {
     res.status(500).json({ error: "failed to load data" });
