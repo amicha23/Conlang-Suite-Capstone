acd9e077dbd675abd28dd0810f8ad0a9f0a1adb1
Bryan Ahaneku <btac3ix3@uw.edu>
2023-03-14 11:48:42 -0700

    centered login and added img folder

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,12 @@
 // import 'antd/dist/antd.css';
+// import ScriptTag from 'react-script-tag';
+import 'bootstrap/dist/css/bootstrap.css';
 import { Input } from 'antd';
 import { GoogleOutlined, UploadOutlined } from '@ant-design/icons';
 import { Button, message, Upload } from 'antd';
 import React, { useEffect, useState } from "react";
 import { Layout } from 'antd';
+
 const { Header, Footer, Sider, Content } = Layout;
 
 //icons
@@ -21,12 +24,13 @@ export default function login() {
 
 
   return (
-      <div>
-          <Layout>            
-            <Content style={{ padding: '0 20px', background: 'white'}}>
+      <div className="container">
+        <section className="d-flex justify-content-center" >
+            <div /* </Layout>style={{ padding: '0 20px', background: 'white'}}*/>
+              <img src="https://www.iths.org/resources/wp-content/sabai/sites/9/File/files/0bb1f4cb5ff99dd034602ced0e2534fc.png" alt="website logo"/>
               <h1>Log in to your account</h1>
-              <p>Welcome! Please enter your details</p>
-              <div id="first-page-setup">
+              <p >Welcome! Please enter your details</p>
+              <div  id="first-page-setup">
                 <p>Email</p>
                 <Input id="email" placeholder="Email" onChange={e => { setValue(e.currentTarget.value); }}/>
                 <p>Password</p>
@@ -39,14 +43,10 @@ export default function login() {
                 <Button type="primary" onClick={() => saveUserInfo(email, password)}>Sign in</Button>
                 </div>
                 <Button icon={<GoogleOutlined />}>Sign in with Google</Button>
-                
-
-                
-              </div>
-
-              
-            </Content>
-          </Layout>
+              </div>              
+            </div >
+        </section>
       </div>
+      
   );
 }
