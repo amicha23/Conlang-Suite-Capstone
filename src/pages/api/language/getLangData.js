9f5052ab8682f212e143f56399ec1baf7610ca2d
JoJoGong <ugong@uw.edu>
2023-04-26 18:46:58 -0700

    edit getLangData & exportLangHTML

--- src/pages/api/language/getLangData.js
+++ src/pages/api/language/getLangData.js
@@ -4,15 +4,19 @@ import { ref, get } from "firebase/database";
 export default async function getLangData(data) {
   try {
     var lid = data.lid;
-    const dictRef = ref(db, `languages/${lid}`);
+    const langRef = ref(db, `languages/${lid}`);
 
-    const snapshot = await get(dictRef);
+    const snapshot = await get(langRef);
     if (!snapshot.exists()) {
       return "No data available";
     }
 
-    const dict = snapshot.val();
-    return dict;
+    const lang = snapshot.val();
+
+    const dictHeaders = Object.keys(lang.dict);
+    const result = [lang, dictHeaders]
+    // console.log('result[0] :>> ', result[0]);
+    return result;
   } catch (error) {
     return error;
   }
