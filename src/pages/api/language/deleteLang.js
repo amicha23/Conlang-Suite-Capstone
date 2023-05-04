f3acb4b901f33c66fb9e9958623b3b3521113e2d
JoJoGong <ugong@uw.edu>
2023-05-03 17:12:17 -0700

    feature: add scheduled task

--- src/pages/api/language/deleteLang.js
+++ src/pages/api/language/deleteLang.js
@@ -40,11 +40,39 @@ export default async function deleteLang(data) {
     updates[`languages/${lid}`] = null;
     updates[`users/${uid}/lid`] = user_lid;
 
+    const currentTime = getCurrTime();
+    langData["deleteTime"] = currentTime;
+
     set(ref(db, `deleteRecord/${uid}/${lid}`), { langData });
 
     await update(ref(db), updates);
-    return "Success"
+    return "Success";
   } catch (e) {
     return e;
   }
+}
+
+function getCurrTime() {
+  let date_ob = new Date();
+
+  let date = ("0" + date_ob.getDate()).slice(-2);
+  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+  let year = date_ob.getFullYear();
+  let hours = date_ob.getHours();
+  let minutes = date_ob.getMinutes();
+  let seconds = date_ob.getSeconds();
+  let currentTime =
+    year +
+    "-" +
+    month +
+    "-" +
+    date +
+    " " +
+    hours +
+    ":" +
+    minutes +
+    ":" +
+    seconds;
+
+  return currentTime;
 }
